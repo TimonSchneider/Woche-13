6094bd36382382f61bf7f79b8191acd33c5453c5
timonschneider <timon.schneider@hotmail.com>
2023-06-04 14:00:46 +0200

    änderung in javascript für image built

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express(); 
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course :-)'); 
+res.send('Hello Timon Schneider / Neue änderungen'); 
 });
 
 app.listen(PORT, HOST); 
